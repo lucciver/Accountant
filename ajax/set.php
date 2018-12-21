b5265bef265f89c867cf23e5a10414ca763e9ab6
Sjors van Holst <sjorsvanholst@gmail.com>
2018-12-21 15:08:51 +0100

    Added ajax set

---
+++ ajax/set.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $database = require('config.php');
+
+  if (isset($_POST['id'])) {
+    $stmt = $database->prepare("UPDATE `expenses` SET `amount` = :amount WHERE `id` = :id");
+    $stmt->bindParam(':amount', $_POST['amount']);
+    $stmt->bindParam(':id', $_POST['id']);
+    $stmt->execute();
+  } else {
+    $stmt = $database->prepare("INSERT INTO `expenses` (`friend`, `amount`, `description`) VALUES (:friend, :amount, :description)");
+    $stmt->bindParam(':friend', $_POST['friend']);
+    $stmt->bindParam(':description', $_POST['description']);
+    $stmt->bindParam(':amount', $_POST['amount']);
+    $stmt->execute();
+  }
+}
+?>
