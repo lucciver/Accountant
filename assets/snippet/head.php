18bf167dba44969a8c30256035d4dfc1b8edb4c2
Sjors van Holst <sjorsvanholst@gmail.com>
2019-01-16 22:48:04 +0100

    Fixed header

--- assets/snippet/head.php
+++ assets/snippet/head.php
@@ -2,5 +2,5 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<script src="script.min.js"></script>
-<link rel="stylesheet" href="style.min.css">
+<script src="/script.min.js"></script>
+<link rel="stylesheet" href="/style.min.css">
